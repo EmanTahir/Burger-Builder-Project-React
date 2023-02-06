406e01a7789433703cc810b3b7dcd8c1dbfc4863
EmanTahir <emantahir555@example.com>
2023-02-06 16:49:36 +0500

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24"/>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    )
+}
