d65b26d296e7f3c6ba5bbd48f9c367824080b22a
EmanTahir <emantahir555@example.com>
2023-02-06 17:39:35 +0500

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './burger-logo.png';
 
 function App() {
+  
   return (
     <>
-    
+      <nav className="navbar bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="Burger Builder" width="50" height="50" />
+          </a>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item" style={{display: 'inline-block', width: 'auto', margin: '0 10px'}}>
+              <a className="nav-link" href="#">Menu 1</a>
+            </li>
+            <li className="nav-item" style={{display: 'inline-block', width: 'auto', margin: '0 10px'}}>
+              <a className="nav-link" href="#">Menu 2</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 }
