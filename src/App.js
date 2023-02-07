4523fefcf2bd513870a33906f149ca2999b1c11e
EmanTahir <emantahir555@example.com>
2023-02-07 22:08:38 +0500

    navbar menus inline and color added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ function App() {
   
   return (
     <>
-      <nav className="navbar bg-body-tertiary">
-        <div className="container">
+      <nav className="navbar " style={{backgroundColor: '#703b09'}}>
+        <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img src={logo} alt="Burger Builder" width="50" height="50" />
           </a>
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav ml-auto" style={{display: 'flex', flexDirection: 'row'}}>
             <li className="nav-item" style={{display: 'inline-block', width: 'auto', margin: '0 10px'}}>
-              <a className="nav-link" href="#">Menu 1</a>
+              <a className="nav-link" href="#" style={{color: 'white'}}>Burger Builder</a>
             </li>
             <li className="nav-item" style={{display: 'inline-block', width: 'auto', margin: '0 10px'}}>
-              <a className="nav-link" href="#">Menu 2</a>
+              <a className="nav-link" href="#" style={{color: 'white'}}>Login</a>
             </li>
           </ul>
         </div>
