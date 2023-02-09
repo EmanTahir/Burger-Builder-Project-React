84657f9bf1cf4188457e464f4a58e64c6db44554
EmanTahir <emantahir555@example.com>
2023-02-09 17:29:01 +0500

    Footer Component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import Navbar from './components/Navbar';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   
   return (
     <>
       <Navbar/>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+export default function Footer() {
+  return (
+    <div className='container-fluid' style={{height: '40vh', backgroundColor: '#cf8f2e',position: 'relative', top: '80vh'}}>
+      
+    </div>
+  )
+}
