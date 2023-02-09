f1a49e9d9ccc76d086fc5d02b5482f169de1c227
EmanTahir <emantahir555@example.com>
2023-02-09 17:44:05 +0500

    Footer Text added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,15 @@ import React, { useState } from 'react'
 
 export default function Footer() {
   return (
-    <div className='container-fluid' style={{height: '40vh', backgroundColor: '#cf8f2e',position: 'relative', top: '80vh'}}>
-      
+    <div className='container-fluid' style={{height: '40vh', backgroundColor: '#cf8f2e',position: 'relative', top: '80vh', display: 'flex'}}>
+      <section style={{display: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <div>
+        Current Price:
+        </div> 
+        <div>
+            swadwd
+        </div>
+      </section>
     </div>
   )
 }
