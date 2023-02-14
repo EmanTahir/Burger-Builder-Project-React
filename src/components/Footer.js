201e0c84653d4b88ec803e0a860509c0da8223db
EmanTahir <emantahir555@example.com>
2023-02-14 18:33:04 +0500

    Added ingredients

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 
 export default function Footer() {
+  const [currprice, setCurrprice] = useState(3.00)
   return (
-    <div className='container-fluid' style={{height: '40vh', backgroundColor: '#cf8f2e',position: 'relative', top: '80vh', display: 'flex'}}>
-      <section style={{display: 'column', justifyContent: 'center', alignItems: 'center'}}>
+    <div className='container-fluid' style={{ height: '40vh', backgroundColor: '#cf8f2e', position: 'relative', top: '80vh', display: 'flex' }}>
+      <section style={{ display: 'row', alignContent: 'center', alignItems: 'center' }}>
         <div>
-        Current Price:
-        </div> 
-        <div>
-            swadwd
+          Current Price: <strong>$ {currprice} </strong>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <strong>Lettuce</strong>
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+          </div>
         </div>
       </section>
     </div>
