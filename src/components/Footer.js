a5058711e28c679d8d2f0944a2ae4d8386611993
EmanTahir <emantahir555@example.com>
2023-03-08 20:14:20 +0500

    Footer front view completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,53 @@
 import React, { useState } from 'react'
+import '../App.css';
+
 
 export default function Footer() {
   const [currprice, setCurrprice] = useState(3.00)
   return (
-    <div className='container-fluid' style={{ height: '40vh', backgroundColor: '#cf8f2e', position: 'relative', top: '80vh', display: 'flex' }}>
-      <section style={{ display: 'row', alignContent: 'center', alignItems: 'center' }}>
+    <div className='container-fluid footerStyle justify-content-center' >
+      <section className='burgerItems'>
         <div>
-          Current Price: <strong>$ {currprice} </strong>
+          <p style={{textAlign: 'center'}}>Current Price: <strong>${currprice} </strong> </p>
         </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm">
-              <strong>Lettuce</strong>
-            </div>
-            <div class="col-sm">
-              One of three columns
-            </div>
-            <div class="col-sm">
-              One of three columns
-            </div>
+        <div className="container d-flex justify-content-center">
+          <div className="row">
+            <div className="col-3"><p><strong>Lettuce</strong></p></div>
+            <div className="col-4"><button className='bLess' >Less</button></div>
+            <div className="col-4"><button className='bMore'>More</button></div>
           </div>
         </div>
+
+        <div className="container d-flex justify-content-center">
+          <div className="row">
+            <div className="col-3"><p><strong>Bacon</strong></p></div>
+            <div className="col-4"><button className='bLess' >Less</button></div>
+            <div className="col-4"><button className='bMore'>More</button></div>
+          </div>
+        </div>
+
+        <div className="container d-flex justify-content-center">
+          <div className="row">
+            <div className="col-3"><p><strong>Cheese</strong></p></div>
+            <div className="col-4"><button className='bLess' >Less</button></div>
+            <div className="col-4"><button className='bMore'>More</button></div>
+          </div>
+        </div>
+
+        <div className="container d-flex justify-content-center">
+          <div className="row">
+            <div className="col-3"><p><strong>Meat</strong></p></div>
+            <div className="col-4"><button className='bLess' >Less</button></div>
+            <div className="col-4"><button className='bMore'>More</button></div>
+          </div>
+        </div>
+
+        <div className="container d-flex justify-content-center">
+          <div className="row">
+            <div className="col-11"><button type="button" class="orderBtn text-nowrap">Sign Up to Order</button></div>
+          </div>
+        </div>
+
       </section>
     </div>
   )
